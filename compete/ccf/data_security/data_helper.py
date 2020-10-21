77faf4da1ec3f413f9af9d0913119435fe91c4a4
Charlie <zhangchen143@gmail.com>
2020-10-21 21:47:01 +0800

    init commit

---
+++ compete/ccf/data_security/data_helper.py
@@ -0,0 +1,49 @@
+import logging
+
+import pandas as pd
+from tqdm import tqdm
+
+
+from preprocess.feature.transform import *
+from preprocess.text.tokenize import *
+from util.eda import *
+
+
+tqdm.pandas()
+
+
+def load_labelled_data(path):
+    """
+    header: id,class_label,content
+    :param path:
+    :return:
+    """
+    df = pd.read_csv(path)
+
+    df["content_seq"] = df["content"].progress_apply(jieba_tokenize)
+    # feature_selection(df["content_seq"], df["class_label"])
+    seq_stat = SequenceStatistics(df["content_seq"])
+    logging.info(seq_stat)
+    return df
+
+
+def load_unlabelled_data(path):
+    """
+    header: id,content
+    :param path:
+    :return:
+    """
+    df = pd.read_csv(path)
+
+
+def load_test_data(path):
+    """
+    header: id,content
+    :param path:
+    :return:
+    """
+    df = pd.read_csv(path)
+
+
+if __name__ == '__main__':
+    load_labelled_data(r'C:\Users\charlie\developer\data\nlp\compete\ccf\2020\面向数据安全治理的数据内容智能发现与分级分类\labeled_data.csv')
