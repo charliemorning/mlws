9916557e6d816d3f8a69c8d030e81ea7726bd3aa
Charlie <zhangchen143@gmail.com>
2021-01-08 00:07:48 +0800

    add embedding matrix builder

--- util/model.py
+++ util/model.py
@@ -3,6 +3,7 @@ import pickle
 import os
 import logging
 
+from preprocess.feature.transform import UNKNOWN
 
 def load_embedding_index(path: str, ext="pickle"):
 
@@ -28,3 +29,33 @@ def load_embedding_index(path: str, ext="pickle"):
             pickle.dump(embedding_index, f)
 
     return embedding_index
+
+
+def rebuild_embedding_index(embedding_index, word_index):
+
+    new_embedding_index = {}
+    for word in word_index:
+        if word in embedding_index:
+            new_embedding_index[word] = embedding_index[word]
+
+    return new_embedding_index
+
+
+def build_embedding_matrix(embedding_index, word_index, oov_strategy=None):
+
+    # get embedding size
+    for embedding in embedding_index.values():
+        embedding_size = len(embedding)
+        break
+
+    embedding_matrix = np.zeros((len(word_index) + 1, embedding_size), dtype=np.float)
+
+    for word, i in word_index.items():
+        if word in embedding_index:
+            embedding_matrix[i] = embedding_index[word]
+        else:
+            embedding_matrix[i] = np.random.randn(embedding_size)
+
+    return embedding_matrix
+
+
