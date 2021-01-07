17c54d03df04e41afd432991b06beb361f10600b
Charlie <zhangchen143@gmail.com>
2021-01-08 00:10:08 +0800

    refactor package

--- tasks/kaggle/disaster_tweets/torch_bert_ft.py
+++ tasks/kaggle/disaster_tweets/torch_bert_ft.py
@@ -1,18 +1,15 @@
 import os
-import pandas as pd
 
-from models.nlp.framework import SupervisedNNModelTrainConfig
-from models.nlp.bert.pt.text_cls_ft_trainer import BertModelConfig, BertTrainDataset, QuickBertTrainer, BertTrainer
+from models.torch.trainer import SupervisedNNModelTrainConfig
+from models.torch.nlp.network.bert.text_cls_ft import BertModelConfig, TextClsCustomedBertTrainer
 from util.corpus import train_test_split_from_data_frame
+from tasks.kaggle.disaster_tweets import prepare_data
 
-
-DATA_HOME = r"C:\Users\Charlie\Corpus\kaggle\nlp-getting-started"
 MODEL_HOME = r"L:\developer\model\bert-base-uncased"
 
 
 def main():
-    df = pd.read_csv(os.path.join(DATA_HOME, "train.csv"))
-    df["target"] = df["target"].apply(lambda x: int(x))
+    df = prepare_data()
     train_df, dev_df = train_test_split_from_data_frame(df)
 
     xs_train = train_df["text"].tolist()
@@ -25,12 +22,13 @@ def main():
     # label2id = {label: i for i, label in enumerate(label_list)}
 
     train_config = SupervisedNNModelTrainConfig(
-        epoch=7,
+        epoch=20,
         train_batch_size=64,
         eval_batch_size=64,
         logging_dir="L:/tmp/logging",
         output_dir="L:/tmp/output",
-        warmup_steps=500
+        warmup_steps=500,
+        dim_out=1
     )
 
     bert_model_config = BertModelConfig(
@@ -38,11 +36,11 @@ def main():
         tokenizer_path=os.path.join(MODEL_HOME, "vocab.txt"),
         model_path=os.path.join(MODEL_HOME, "pytorch_model.bin"),
         cache_dir="L/tmp/cache",
-        freeze_pretrained_model_while_training=True
+        freeze_pretrained_model_while_training=False
     )
 
-    trainer = BertTrainer(train_config=train_config, bert_model_config=bert_model_config)
-    trainer.fit(train_data=(xs_train, ys_train), valid_data=(xs_dev, ys_dev))
+    trainer = TextClsCustomedBertTrainer(train_config=train_config, bert_model_config=bert_model_config)
+    trainer.fit(train_data=(xs_train, ys_train), eval_data=(xs_dev, ys_dev))
 
 
 if __name__ == "__main__":
