8bf7f4545d3998759dd8504d6ee5e2f6f41b84bc
Charlie <zhangchen143@gmail.com>
2021-08-06 00:17:58 +0800

    add one hot label encoder

--- util/label.py
+++ util/label.py
@@ -6,4 +6,15 @@ from sklearn.preprocessing import LabelEncoder
 def encode_labels(ys):
     le = LabelEncoder()
     encoded_labels = le.fit_transform(ys)
-    return np.unique(encoded_labels).astype("str")
+    return np.unique(encoded_labels).astype("str"), encoded_labels
+
+
+def encode_onehot_labels(ys, labels_index, encoded_labels):
+    if labels_index is None or encoded_labels is None:
+        labels_index, encoded_labels = encode_labels(ys)
+    onehot_labels = np.zeros((len(ys), len(labels_index)))
+    for i, col_i in enumerate(encoded_labels):
+        onehot_labels[i][col_i] = 1
+    return onehot_labels
+
+
