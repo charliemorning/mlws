08b92529b5f668843cd3094d42af36a226f7f434
Charlie <zhangchen143@gmail.com>
2021-08-10 00:22:26 +0800

    fix unigram representation

--- misc/lm.py
+++ misc/lm.py
@@ -2,6 +2,22 @@ from gensim.test.utils import common_texts
 from gensim.models import Word2Vec, FastText
 import json
 
+
+START_TOKEN = "<s>"
+END_TOKEN = "<e>"
+
+
+def tokenize(text, n):
+
+    expand_tokens = [START_TOKEN] + text + [END_TOKEN]
+    ngrams = []
+    for i in range(len(text) + 1):
+        ngram = tuple(expand_tokens[i:i + n])
+        ngrams.append(str(ngram))
+
+    return ngrams
+
+
 # datagrand_2021_unlabeled_data.json
 class MySentences(object):
     def __init__(self, filepath):
@@ -14,12 +30,12 @@ class MySentences(object):
             content = obj["content"].split()
             text = title + content
             # print(text)
-            yield text
+            yield tokenize(text, 2)
 
 
 sentences = MySentences("L:/developer/datagrand_2021_unlabeled_data.json")
 
-model = Word2Vec(sentences=sentences, vector_size=300, window=5, min_count=1, workers=4)
+model = Word2Vec(sentences=sentences, vector_size=300, window=5, min_count=5, workers=8)
 
 
-model.save("L:/developer/word2vec.skipgram.unigram.300d.model")
+model.save("L:/developer/word2vec.skipgram.bigram.300d.model")
